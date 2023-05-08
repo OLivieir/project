516bfdb9b9fffe10f799c8c2e30e5b7e8fd8d444
Zosia Samosia <Zosia@Samosia.pl>
2023-05-08 12:45:34 +0200

    costam

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const btn = document.querySelector('header a');
+const h1 = document.querySelector('header h1');
+const h1text = h1.innerHTML;
+let counter = 0;
+btn.addEventListener('click' , () =>{
+    counter++;
+//     if(counter % 2 === 0){
+//         h1.innerText = h1text;
+//     }else{
+//     h1.innerText = "będzie Pan Zadowolony :)"
+// }
+counter % 2 === 0 ? h1.innerText = h1text : h1.innerText = "Będzie Pan Zadowolony :)"
+
+});
